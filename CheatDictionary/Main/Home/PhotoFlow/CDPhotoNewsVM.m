51346debca0563ffe4b0ce83a01168afbea97248
zhengyi <logic_wy@163.com>
2019-03-02 14:50:53 +0800

    图片流使用json占位数据

--- CheatDictionary/Main/Home/PhotoFlow/CDPhotoNewsVM.m
+++ CheatDictionary/Main/Home/PhotoFlow/CDPhotoNewsVM.m
@@ -16,36 +16,50 @@
     
     [CDApiClient GET:@"photo_flow" success:^(NSDictionary *data) {
         
-        NSArray *items = [data objectForKey:@"items"];
-        
-        NSMutableArray *mutableArray = [NSMutableArray array];
-        
-        for (NSDictionary *item in items) {
-            CDSectionModel *sectionModel = [CDSectionModel new];
-            CDShowMoreHeaderModel *header = [CDShowMoreHeaderModel new];
-            header.title = item[@"title"];
-            sectionModel.headerModel = header;
-            sectionModel.objects = [NSMutableArray array];
-            NSArray *channels = item[@"items"];
-            for (NSDictionary *channel in channels) {
-                CDPhotoFlowNewsModel *channelModel = [CDPhotoFlowNewsModel modelWithJSON:channel];
-                [sectionModel.objects addObject:channelModel];
-            }
-            [mutableArray addObject:sectionModel];
-        }
-        
-        self.objects = mutableArray;
-        
+        [self _parseData:data];
         if (self.completeLoadDataBlock) {
             self.completeLoadDataBlock(YES);
         }
         
     } failure:^(NSInteger code, NSString *message) {
-        
+        if (!self.objects.count) {
+            NSString *jPath = [[NSBundle mainBundle] pathForResource:@"photo_flow" ofType:@"json"];
+            NSDictionary *jDic = [NSJSONSerialization JSONObjectWithData:[NSData dataWithContentsOfFile:jPath] options:NSJSONReadingMutableLeaves error:nil];
+            NSDictionary *data = [jDic objectForKey:@"data"];
+            
+            [self _parseData:data];
+            if (self.completeLoadDataBlock) {
+                self.completeLoadDataBlock(YES);
+            }
+            return;
+        }
         if (self.completeLoadDataBlock) {
             self.completeLoadDataBlock(NO);
         }
     }];
 }
+
+- (void)_parseData:(NSDictionary *)data {
+    NSArray *items = [data objectForKey:@"items"];
+    
+    NSMutableArray *mutableArray = [NSMutableArray array];
+    
+    for (NSDictionary *item in items) {
+        CDSectionModel *sectionModel = [CDSectionModel new];
+        CDShowMoreHeaderModel *header = [CDShowMoreHeaderModel new];
+        header.title = item[@"title"];
+        sectionModel.headerModel = header;
+        sectionModel.objects = [NSMutableArray array];
+        NSArray *channels = item[@"items"];
+        for (NSDictionary *channel in channels) {
+            CDPhotoFlowNewsModel *channelModel = [CDPhotoFlowNewsModel modelWithJSON:channel];
+            [sectionModel.objects addObject:channelModel];
+        }
+        [mutableArray addObject:sectionModel];
+    }
+    
+    self.objects = mutableArray;
+}
+
 
 @end
